d76b4c7bcbec81b2ed0e05a58a87eaa90936ea7b
Thobio Joseph <jthobio2@gmail.com>
2022-04-03 13:25:45 +0530

    Initial Commit

---
+++ Tawk Ios Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tawk Ios Project
+//
+//  Created by Thobio Joseph on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tawk Ios ProjectUITests/Tawk_Ios_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tawk_Ios_ProjectUITestsLaunchTests.swift
+//  Tawk Ios ProjectUITests
+//
+//  Created by Thobio Joseph on 03/04/22.
+//
+
+import XCTest
+
+class Tawk_Ios_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
